f023773a789b2b51b6ae0f28c9c39942d5b57b7c
Dmitry-DVal <149782954+Dmitry-DVal@users.noreply.github.com>
2024-05-16 15:45:49 +0400

    Added next_tern()
    Now entities can simply move to the assigned coordinate

--- Entities/Grass.py
+++ Entities/Grass.py
@@ -4,8 +4,9 @@ class Grass(Entity): # Трава
     "Класс Трава, может быть съеден травоядными"
 
     def __str__(self):
-        return 'The object Grass'
+        return f'The object {self.name}'
 
     def __init__(self, coordinate):
         super().__init__(coordinate)
         self.image = '🌿'
+        self.name = 'Grass'
