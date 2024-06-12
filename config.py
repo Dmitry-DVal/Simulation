58f060a955b8c558d3c282475a341bf7ad94a7c8
Dmitry-DVal <149782954+Dmitry-DVal@users.noreply.github.com>
2024-06-12 17:48:27 +0400

    Added config with game parameters

---
+++ config.py
@@ -0,0 +1,22 @@
+class Config:
+    # Размер Поля
+    width = 15  # Y
+    height = 8  # X
+
+    # Кол-во существ
+    predatorNumber = 2  # 5
+    herbivoreNumber = 2  # 6
+    treeNumber = 12  # 7
+    grassNumber = 3  # 10
+    rockNumber = 12  # 7
+
+    # Скорость существ
+    predatorSpeed = 5
+    herbivoreSpeed = 3
+
+    # Здоровье существ
+    predatorHp = 7
+    herbivoreHp = 7
+
+    # Урон хищника
+    predatorDamage = 5
